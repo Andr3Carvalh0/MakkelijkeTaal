a4a1193e25bc8a518f65dfe6f953b8f6c295c741
André Carvalho <andre.daniel.carvalho@gmail.com>
2025-04-28 21:57:12 +0200

    fix: Add missing curl command to Dockerfile

--- docker/run.sh
+++ docker/run.sh
@@ -9,7 +9,7 @@ rm -rf node_modules
 npm install
 npm run main
 
-git add .
+git add podcast.rss
 if [[ -n "$(git status --porcelain)" ]]; then
     git commit -m "Import episode of $(date +"%d %B")"
     git push
