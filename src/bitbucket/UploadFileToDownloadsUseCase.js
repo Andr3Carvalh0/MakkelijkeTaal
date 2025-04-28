7be6f90b97761781422c0003fdf0cbdfcdbd513d
André Carvalho <andre.daniel.carvalho@gmail.com>
2025-04-28 21:47:13 +0200

    refactor: Docker

--- src/bitbucket/UploadFileToDownloadsUseCase.js
+++ src/bitbucket/UploadFileToDownloadsUseCase.js
@@ -2,5 +2,6 @@ import { BITBUCKET_BUCKET, BITBUCKET_TOKEN } from "../common/Constants.js"
 import { execute } from "../utilities/TerminalUseCase.js"
 
 export const invoke = async (path) => {
+    console.log(`Uploading file from ${path}, with token: ${BITBUCKET_TOKEN}, url: ${BITBUCKET_BUCKET}`)
     return execute(`curl -s --header \"Authorization: Bearer ${BITBUCKET_TOKEN}\" -X POST ${BITBUCKET_BUCKET} -F files=@${path}`)
 }
--- src/utilities/TerminalUseCase.js
+++ src/utilities/TerminalUseCase.js
@@ -3,7 +3,10 @@ import { exec } from 'child_process'
 export function execute(command){
     return new Promise((resolve, reject) => {
         exec(command, (error, stdout, stderr) => {
-            if (error !== null) reject(error)
+            if (error !== null) {
+                console.error(`CLI command has failed: ${error}`)
+                reject(error)
+            }
             resolve(stdout)
         })
     })
